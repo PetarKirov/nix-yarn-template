4bfe5ea1edf49906e6deb871a9f9f110083ec7ba
Petar Kirov <petar.p.kirov@gmail.com>
2022-03-20 00:21:36 +0200

    improve(apps/cli): Replace boilerplate code with simple async main function

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,1 +1,10 @@
-export * from './lib/cli';
+export async function main() {
+  console.log('Hello world')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  })
